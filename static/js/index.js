d164f0c68505ed5da7eeb631ad0e498698eec8cf
BenBrookfield <Benbrookyboy@gmail.com>
2024-09-06 04:44:57 +0100

    updated project section on resume page

--- static/js/index.js
+++ static/js/index.js
@@ -8,6 +8,28 @@ projectsButton.onclick = function(){
     document.querySelectorAll(".bottom").forEach(element => element.remove());
 
     // Create all entities for this section and add them to "bottom" class
+    const heading4 = document.createElement("h2");
+    heading4.classList.add("Bottom");
+    const link4 = document.createElement("a");
+    link4.classList.add("bottom");
+    const link5 = document.createElement("a");
+    link5.classList.add("bottom");
+    const para5 = document.createElement("p");
+    para5.classList.add("bottom");
+    const para6 = document.createElement("p");
+    para6.classList.add("bottom");
+    const line3 = document.createElement("hr");
+    line3.classList.add("bottom");
+
+    const heading3 = document.createElement("h2");
+    heading3.classList.add("bottom");
+    const link3 = document.createElement("a");
+    link3.classList.add("bottom");
+    const para4 = document.createElement("p");
+    para4.classList.add("bottom");
+    const line2 = document.createElement("hr");
+    line2.classList.add("bottom");
+
     const heading1 = document.createElement("h2");
     heading1.classList.add("bottom");
     const link1 = document.createElement("a");
@@ -19,12 +41,71 @@ projectsButton.onclick = function(){
     para2.classList.add("bottom");
     const line1 = document.createElement("hr");
     line1.classList.add("bottom");
+
     const heading2 = document.createElement("h2");
     heading2.classList.add("bottom");
     const link2 = document.createElement("a");
     link2.classList.add("bottom");
     const para3 = document.createElement("p");
     para3.classList.add("bottom");
+
+    heading4.textContent = "PlaygrounDB - Online SQL practice tool"
+    document.body.appendChild(heading4);
+
+    link4.textContent = "Try PlaygrounDB"
+    link4.href = "http://www.benbrookfield.me/playgroundDB"
+    document.body.appendChild(link4);
+
+    const lbreak = document.createElement("br");
+    lbreak.classList.add("bottom")
+    document.body.appendChild(lbreak);
+
+    link5.textContent = "Github link"
+    link5.href = "https://github.com/benbrookfield/benbrookfield.github.io"
+    document.body.appendChild(link5);
+
+    para5.textContent = "PlaygrounDB is my most recent and favourite project. Built with pythons Flask framework, HTML," +
+        ' CSS, and SQLite, PlaygrounDB is a full stack web application which allows users to query a sample SQLite ' +
+        'database and presents the query results in a table on the website for you. It is designed to help people learn ' +
+        'SQL without needing to download a database or any software.'
+    document.body.appendChild(para5);
+
+    para6.textContent = "Building playgrounDB felt like a natural progression from my previous projects, which had used " +
+        "HTML, CSS, Python and SQLite but never combined them all into a single project. It also allowed me to build " +
+        "upon this webpage, which previously only contained HTML, CSS and Javascript files. Adding in a backend was really " +
+        "interesting and fun. I am really proud of this project and my favourite thing about it is definitely being able " +
+        "to go onto the website anywhere I want and show it off to friends and family!"
+    document.body.appendChild(para6);
+    document.body.appendChild(line3);
+
+
+    heading3.textContent = 'Folder cleanup tool'
+    document.body.appendChild(heading3);
+
+    link3.textContent = 'Github link'
+    link3.href = "https://github.com/benbrookfield/File_cleanup_tool"
+    document.body.appendChild(link3);
+
+    para4.textContent = 'During my first-year christmas break I spent a weekend making a file cleanup tool. This tool goes through' +
+        ' a folder on a computer (I have it set to my own downloads folder in the code) and iterates through all loose files. It ' +
+        'checks the file type of all files and moves them to a new folder based on their file format. This project was ' +
+        'super easy and fun, and is also one I actually use when my downloads folder is too cluttered!'
+    document.body.appendChild(para4);
+    document.body.appendChild(line2);
+
+    heading2.textContent = 'Portfolio Website'
+    document.body.appendChild(heading2);
+
+    link2.textContent = 'Github link'
+    link2.href = "https://github.com/benbrookfield/portfolio-website"
+    document.body.appendChild(link2);
+
+    para3.textContent = 'My second notable project is this website! Although the website is quite basic, creating it was' +
+        ' quite a challenge as I had no previous experience with HTML, CSS, or Javascript prior to making this and had' +
+        ' to teach myself how to use them with the help of youtube, W3Schools and Stack Overflow. I have found the ' +
+        ' development process of this website very fun, and really enjoyed learning the new languages.'
+    document.body.appendChild(para3);
+    document.body.appendChild(line1);
 
     // Add text and any other neccesarry info e.g. href links to all entities, then append them.
     heading1.textContent = "Fitassist - A Level Coursework"
@@ -47,21 +128,6 @@ projectsButton.onclick = function(){
         ' major project. I had lots of fun making it and was felt a great sense of pride once it was completed, which' +
         ' really motivated me to expand my skills so I could make more impressive projects in the future.'
     document.body.appendChild(para2);
-
-    document.body.appendChild(line1);
-
-    heading2.textContent = 'Portfolio Website'
-    document.body.appendChild(heading2);
-
-    link2.textContent = 'Github link'
-    link2.href = "https://github.com/benbrookfield/portfolio-website"
-    document.body.appendChild(link2);
-
-    para3.textContent = 'My second notable project is this website! Although the website is quite basic, creating it was' +
-        ' quite a challenge as I had no previous experience with HTML, CSS, or Javascript prior to making this and had' +
-        ' to teach myself how to use them with the help of youtube, W3Schools and Stack Overflow. I have found the ' +
-        ' development process of this website very fun, and really enjoyed learning the new languages.'
-    document.body.appendChild(para3);
 }
 
 /*This function loads the work experience section after the work experience button is pressed.*/
